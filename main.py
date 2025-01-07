06e43b57b1704529e6174239df02d4dd4383cf94
CJ <166865964+CJ-S7Z777Z@users.noreply.github.com>
2025-01-07 09:53:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
 import os
 import json
 import hmac
 import hashlib
 import qrcode
 import io
-from flask import Flask, request, jsonify, redirect, url_for, render_template
+import secrets
+from flask import Flask, request, jsonify, render_template
 from yookassa import Configuration, Payment
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
@@ -29,8 +29,9 @@ app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
 
-# Специальный ключ для аутентификации запросов от фронтенда
-API_KEY = os.getenv('API_KEY', 'your_api_key')  # Установите свой ключ
+# Генерация API_KEY при запуске сервера
+API_KEY = secrets.token_hex(32)
+print(f"Сгенерированный API ключ: {API_KEY}")
 
 # Максимум билетов на сеанс
 MAX_TICKETS_PER_SESSION = 50
@@ -51,81 +52,6 @@ def generate_qr_code(data):
     img.save(buffer, format="PNG")
     buffer.seek(0)
     return buffer.read()
-
-@app.route('/create-payment', methods=['POST'])
-def create_payment():
-    # Аутентификация по API_KEY
-    auth_header = request.headers.get('Authorization')
-    if not auth_header or auth_header != f"Bearer {API_KEY}":
-        return jsonify({'error': 'Unauthorized'}), 401
-
-    data = request.get_json()
-    required_fields = ['name', 'surname', 'patronymic', 'phone', 'email', 'day', 'time', 'ticketType', 'quantity', 'totalPrice']
-    if not all(field in data for field in required_fields):
-        return jsonify({'error': 'Missing fields'}), 400
-
-    # Проверка количества доступных билетов
-    session_key = f"{data['day']}-{data['time']}"
-    if session_key not in current_tickets_sold:
-        current_tickets_sold[session_key] = 0
-
-    quantity = int(data['quantity'])
-    if current_tickets_sold[session_key] + quantity > MAX_TICKETS_PER_SESSION:
-        return jsonify({'error': 'К сожалению, на этот сеанс уже нет свободных билетов.'}), 400
-
-    current_tickets_sold[session_key] += quantity  # Увеличиваем количество проданных билетов
-
-    # Создание платежа через ЮKassa
-    try:
-        payment = Payment.create({
-            "amount": {
-                "value": data['totalPrice'],
-                "currency": "RUB"
-            },
-            "confirmation": {
-                "type": "redirect",
-                "return_url": "https://hc-grad/"  # Замените на вашу страницу успешной оплаты
-            },
-            "capture": True,
-            "description": f"Покупка билетов: {quantity} шт.",
-            "receipt": {
-                "customer": {
-                    "email": data['email']
-                },
-                "items": [
-                    {
-                        "description": f"Билет: {'На каток' if data['ticketType'] == 'regular' else 'Льготный'}\nДень: {data['day']}\nВремя: {data['time']}",
-                        "quantity": quantity,
-                        "amount": {
-                            "value": data['totalPrice'],
-                            "currency": "RUB"
-                        },
-                        "vat_code": "1",
-                        "payment_subject": "commodity",
-                        "payment_mode": "full_prepayment",
-                        "type": "payment_item"
-                    }
-                ]
-            },
-            "metadata": {
-                "name": data['name'],
-                "surname": data['surname'],
-                "patronymic": data['patronymic'],
-                "phone": data['phone'],
-                "email": data['email'],
-                "day": data['day'],
-                "time": data['time'],
-                "ticketType": data['ticketType'],
-                "quantity": data['quantity']
-            }
-        })
-
-        return jsonify({
-            'confirmation_url': payment.confirmation.confirmation_url
-        })
-    except Exception as e:
-        print(e)
-        return jsonify({'error': 'Ошибка при создании платежа'}), 500
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
